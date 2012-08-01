c5c8da4e7c3487f53254729c63a2f1686c87e454
kpereksta@gmail.com <kpereksta@gmail.com@a471a742-95e1-0921-ccb3-e756502ac650>
2012-08-01 21:02:36 +0000

    Quick update with some small scripts.

--- trunk/RunawayCar/script.lua
+++ trunk/RunawayCar/script.lua
@@ -1,1 +1,23 @@
-os.luadevsplash();
+-- Here's the main script file for our Runaway car game.
+
+-- Load all of the scripts.
+
+dofile("SCRIPTS/LOAD.LUA")
+
+-- This contains test functions for scrolling the street.
+dofile("SCRIPTS/SCROLL.LUA")
+
+
+--This will just show the street to make sure everything is alright
+while true do
+	screen.clear()
+	drawStreet(PLAYER_POS, STREET_HEIGHT, -10)
+	drawStreet(PLAYER_POS, STREET_HEIGHT, -20)
+	drawStreet(PLAYER_POS, STREET_HEIGHT, -30)
+	drawStreet(PLAYER_POS, STREET_HEIGHT, -40)
+	drawStreet(PLAYER_POS, STREET_HEIGHT, -50)
+	drawStreet(PLAYER_POS, STREET_HEIGHT, -60)
+	
+	screen.flip()
+	screen.waitvblankstart()
+end
