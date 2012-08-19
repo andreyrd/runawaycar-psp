fe5dcc09ad6e3787da4726204c7e8fe34dbb4d8d
kpereksta@gmail.com <kpereksta@gmail.com@a471a742-95e1-0921-ccb3-e756502ac650>
2012-08-19 19:02:38 +0000

    The first full release! Many bug fixes! Lots of changes!

--- trunk/RunawayCar/SCRIPTS/crash.lua
+++ trunk/RunawayCar/SCRIPTS/crash.lua
@@ -1,17 +1,16 @@
 function crashed()
-	screen.print(230, 136, "You have CRASHED!", color.new(255, 255, 255))
-	screen.print(230, 148, "Your score was: " .. SCORE, color.new(255, 255, 255))
+	screen.print(GRAF, 240 - string.len("You have CRASHED!")/2 * 16, 100, "You have CRASHED!", color.new(0, 0, 0))
+	screen.print(GRAF, 240 - string.len("Your score was: " .. SCORE)/2 * 16, 140, "Your score was: " .. SCORE, color.new(0, 0, 0))
 	if SCORE > highscore then
-		screen.print(240, 160, "You beat the highscore!", color.new(255, 255, 255))
+		screen.print(GRAF, 240 - string.len("You beat the highscore!")/2 * 16, 180, "You beat the highscore!", color.new(0, 0, 0))
 		highscore = SCORE
 	else
-		screen.print(240, 160, "The Highscore is " .. highscore, color.new(255, 255, 255))
+		screen.print(GRAF, 240 - string.len("The Highscore is " .. highscore)/2 * 16, 180, "The Highscore is " .. highscore, color.new(0, 0, 0))
 	end
-	screen.print(0,0,"Press Cross to exit to menu", color.new(255, 255, 255))
+	screen.print(GRAF, 0,10,"Press Cross to exit to menu", color.new(0, 0, 0))
 	
 	if controls.press("cross") then
-		select = 2
-		PLAYER_POS = 0
+		select = 1
 		gamemode = 1
 	end
 end
